07b192056ed74498a44582a3b5ea89dfe437d320
Thor D. Noe <36196997+thornoe@users.noreply.github.com>
2019-08-21 18:25:13 +0200

    Updated introduction to scraping
    Updated introduction to scraping

--- Exercises/8. Scraping 1 - Data Collection.py
+++ Exercises/8. Scraping 1 - Data Collection.py
@@ -1,3 +1,4 @@
+# General packages
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,50 +8,67 @@ import random
 import datetime
 
 ## Packages for scraping
-import requests, re, time, json  # for connecting to the internet
+import requests, re, tqdm, time, json
 from bs4 import BeautifulSoup  # for parsing
 from selenium import webdriver
-import explore_regex as e_re
-def get(url, iterations=10, check_function=lambda x: x.ok):
-    """This module ensures that your script does not crash from connection errors.
-        that you limit the rate of your calls
-        that you have some reliability check
-        iterations : Define number of iterations before giving up.
-        exceptions: Define which exceptions you accept, default is all.
-    """
-    for iteration in range(iterations):
-        try:
-            "A function that handles the rate of your calls."
-            time.sleep(1) # sleep one second.
-            response = session.get(url)
-            if check_function(response):
-                return response # if succesful it will end the iterations here
-        except exceptions as e: #  find exceptions in the request library requests.exceptions
-            print(e) # print or log the exception message.
-    return None # your code will purposely crash if you don't create a check function later.
 
-## Functions
-pp = p.PrettyPrinter()  # pp.pprint()
-
-%matplotlib inline
 ## plot styles
+%matplotlib inline
 sns.set_style('white')
 plt.style.use('seaborn-white')
 
-# for automatic browsing / screen scraping (not covered in detail here): 'selenium'
-# for behaving responsibly we use: 'time' and 'our minds'
-# We will write our scrapers with basic python,
-#    for larger projects consider looking into the packages scrapy or pyspider
+################################################################################
+#   Self-defined function for efficient and transparent scraping               #
+################################################################################
+s = requests.session()
+s.headers['name'] = 'Thor Donsby Noe (student)' # telling who I am
+s.headers['email'] = 'jwz766@alumni.ku.dk' # allowing them to contact me
+s.headers['User-Agent'] = 'Mozilla/5.0'
+# 'User-Agent' is sometimes required.
+# Tip: change the user agent to a cell phone to obtain more simple formatting of the html, e.g.
+# s.headers['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2_1 like Mac OS X) AppleWebKit/602.4.6 (KHTML, like Gecko) Version/10.0 Mobile/14D27 Safari/602.1'
+s.headers # show headers
 
-# Adress: www.google.com
-# Instructions: /trends?query=social+data+science
-# Header: information send along with the request, including user agent
-#    (operating system, browser), cookies, and prefered encoding.
-#    set to 'smartphone'
+def get(url, iterations=20, sleep_ok=1, sleep_err=5, check_function=lambda x: x.ok):
+    """This module ensures that your script does not crash from connection errors,
+        that you limit the rate of your calls,
+        and that you have some reliability check.
+        iterations : Define number of iterations before giving up.
+    """
+    for iteration in range(iterations):
+        try:
+            time.sleep(sleep_ok) # sleep everytime
+            response = s.get(url) # the s-function defined above
+            if check_function(response):
+                return response # if succesful it will end the iterations here
+        except requests.exceptions.RequestException as e: # find exceptions in the request library requests.exceptions
+            """ Exceptions: Define which exceptions you accept, default is all.
+            For specific errors see:
+            stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module
+            """
+            print(e)  # print or log the exception message
+            time.sleep(sleep_err) # sleep before trying again in case of error
+    return None # code will purposely crash if you don't create a check function later
 
-###############################################################################
-#    Collecting data on display - lets you choose between tables directly     #
-###############################################################################
+################################################################################
+#    Automatic browsing - programming your browser to do stuff on it's own     #
+################################################################################
+"""
+for automatic browsing / screen scraping (not covered in detail here): 'selenium'
+for behaving responsibly we use: 'time' and 'our minds'
+We will write our scrapers with basic python,
+   for larger projects consider looking into the packages scrapy or pyspider
+
+Adress: www.google.com
+Instructions: /trends?query=social+data+science
+Header: information send along with the request, including user agent
+   (operating system, browser), cookies, and prefered encoding.
+   set to 'smartphone'
+"""
+
+################################################################################
+#    Collecting data on display - lets you choose between tables directly      #
+################################################################################
 ## Basic examples
 url = 'https://www.basketball-reference.com/leagues/NBA_2018.html' # link to the website
 dfs = pd.read_html(url)  # parses all tables found on the page.
@@ -62,9 +80,9 @@ dfs[0]  # showing 1st table
 ## search for specific tables
 EC_df = pd.read_html(url,attrs={'id':'confs_standings_E'})  # only parse the tables with attribute confs_standings_E
 
-#############################################
-#   Collecting datas BEHIND the display     #
-#############################################
+################################################################################
+#   Collecting data BEHIND the display                                         #
+################################################################################
 """
 Open (or install) Firefox, right-click on a graphic to "Inspect element".
 Go to "Network" banner, choose XHR, update (F5) and look for JSON files.
@@ -75,7 +93,7 @@ Go to "Network" banner, choose XHR, update (F5) and look for JSON files.
 
 url = 'https://graphs2.coinmarketcap.com/currencies/bitcoin/'
 # response = requests.get(url)
-response = get(url)  # self defined function
+response = get(url)  # self-defined function
 response.ok
 response.text
 d = requests.get(url).json()
@@ -85,75 +103,74 @@ x,y = zip(*d['price_usd'])
 ### Error: shows seconds on January 1 in 1970
 # t = [pd.to_datetime(t) for t in x]
 # t = [datetime.datetime.fromtimestamp(int(i)) for i in x]
-#
 # plt.plot(t,y,label='usd')  # you can add as many as you want of theese!
 # plt.yscale('log')
 # plt.title('Bitcoin price in USD')
 # plt.ylabel('log USD')
 # plt.show()
 
-###############################################################
-#      Collecting unstructured data from kurser.ku.dk         #
-###############################################################
-study_year = '2016-2017'
-def study_boards(study_year):  # from 2013/2014 to 2016/2017
+################################################################################
+#   Collecting unstructured data from kurser.ku.dk                             #
+################################################################################
+"""
+Hint for crating a list via a loop:
+    study_years = [str(y)+"-"+str(y+1) for y in range(first_year, last_year)]
+is just a shorter version of writing:
+    study_years = []
+    for y in range(first_year, last_year):
+        s = str(y)+"-"+str(y+1)
+        study_years.append(s)
+"""
 
+def study_boards(first_year, last_year):
+    """ Input: The first and last year considered
+        e.g. 2013 and 2014 to get the single study year 2013-2014.
+        Output: The links for all of the study boards.
+    """
+    study_years = [str(y)+"-"+str(y+1) for y in range(first_year, last_year)]
+    all_years = []
+    for study_year in tqdm.tqdm(study_years):
+        url = 'https://kurser.ku.dk/archive/'+study_year
+        response = get(url)
+        html = response.text
+        study_board_locations = html.split('href="')[1:]
+        all_links = [study_board_loc.split('"')[0] for study_board_loc in study_board_locations]
+        study_boards = [study_board for study_board in all_links if str(study_year) in study_board]
+        study_board_links = ['https://kurser.ku.dk/archive/'+study_board for study_board in study_boards]
+        all_study_boards = []
+        for study_board_link in tqdm.tqdm(study_board_links):
+            url = str(study_board_link)
+            response = get(url)
+            html = response.text
+            link_locations = html.split('href="')[1:]
+            all_links = [link_loc.split('"')[0] for link_loc in link_locations]
+            links = ['https://www.kurser.ku.dk'+link for link in all_links if '/archive/' in link]
+            all_study_boards += links # += joins two lists
+        all_years += all_study_boards
+    return all_years
 
-url = 'https://kurser.ku.dk/archive/'+str(study_year)
-response = get(url)
-# response.ok
-html = response.text
-study_board_locations = html.split('href="')[1:]
-study_boards = []
-for study_board_loc in study_board_locations:
-    study_board = study_board_loc.split('"')[0]
-    study_boards.append(study_board)
-study_board = [study_board for study_board in study_boards if str(study_year) in study_board]
-study_board_links = ['https://kurser.ku.dk/archive/'+study_board for study_board in study_board]
-
-for study_board_link in study_board_links:
-    url = str(study_board_link)
-    response = get(url)
-    html = response.text
-    link_locations = html.split('href="')[1:]
-    links = []
-    for link_loc in link_locations:
-        link = link_loc.split('"')[0]
-        links.append(link)
-    link = [link for link in links if '/archive/' in link]
-    links = ['https://www.kurser.ku.dk'+link for link in link]
+links = study_boards(first_year = 2015, last_year = 2017)
 
 len(links)
-random.sample(links,5)
+random.sample(links,10)
 
-###############################################################
-#                 How URLS are constructed                    #
-###############################################################
-# / is like folders on your computer.
-# ? entails the start of a query with parameters
-# = defines a variable: e.g. page=1000 or offset = 100 or showNumber=20
-# & separates different parameters.
-# + is html for whitespace
+################################################################################
+#   How URLS are constructed                                                   #
+################################################################################
+"""
+/ is like folders on your computer.
+? entails the start of a query with parameters
+= defines a variable: e.g. page=1000 or offset=100 or showNumber=20
+& separates different parameters.
++ is html for whitespace
+"""
 
-###############################################################
-#                   Transparent scraping                      #
-###############################################################
-response = requests.get('https://www.google.com')
-session = requests.session()
-session.headers['email'] = 'jwz766@alumni.ku.dk'
-session.headers['name'] = 'Thor Donsby Noe'
-
-# session.headers['User-Agent'] = '' # sometimes you need to pose as another agent...
-# e.g. A quick tip is that you can change the user agent to a cellphone
-#  to obtain more simple formatting of the html.
-session.headers
-
-###############################################################
-#                           Exercise 8                        #
-###############################################################
+################################################################################
+#   Exercise 8                                                                 #
+################################################################################
 ### 8.1.2: Collect the 1st page
 url = 'https://job.jobnet.dk/CV/FindWork/Search'
-resp = requests.get(url)        # We use the request module to collect the first page of job postings
+resp = get(url) # We use the request module to collect the first page of job postings
 jobnet = json.loads(resp.text)
 jobnet
 
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Social Data Science 2018
 Summer school course on University of Copenhagen, Department of Economics.
 -  See [our paper](https://github.com/thornoe/sds_2018/blob/master/CPH/Predicting%20apartment%20prices%20in%20the%20urban%20area%20of%20Copenhagen.pdf) "Predicting apartment prices in the urban area of Copenhagen: A machine learning approach".
--  See the [Jupyter Notebook](https://github.com/thornoe/sds_2018/blob/master/CPH/Notebook_final.ipynb) for Python code for using machine learning to predict housing prices.
+-  See the [Jupyter Notebook](https://github.com/thornoe/sds_2018/blob/master/CPH/Notebook_final.ipynb) for our Python code for using machine learning to predict housing prices.
+-  See the [Scraping 1 - Data Collection](https://github.com/thornoe/sds_2018/blob/master/Exercises/8.%20Scraping%201%20-%20Data%20Collection.py) for elaborate Python code to get started with web scraping.
 -  See [abjer/sds](https://github.com/abjer/sds) for the public course material.
 
 ## Getting started with Atom
